396e3da01ef6521ca6a74bd26e1cbb234e56b27c
sufianWG <webgurumrmbd@gmail.com>
2026-08-03 14:30:52 +0600

    connected to mongdodb & added post functionalities

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const express = require('express');
 const dotenv = require("dotenv");
+const {MongoClient} = require("mongodb");
+const cors = require("cors");
 const app = express();
 dotenv.config();
-const port = process.env.PORT 
+
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.PORT || 5260;
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+ async function connectToMongoDB() {
+  try {
+    await client.connect();
+
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection("destinations");
+
+    app.post("/destination", async(req, res) => {
+        const destination = req.body
+        console.log(destination);
+
+        const result = await destinationCollection.insertOne(destination);
+        res.send(result);
+        console.log("This is destination endpoint");
+    })
+
+    console.log("You successfully connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+connectToMongoDB()
 
 app.get('/', (req, res) => {
     console.log("Server runnig fine");
